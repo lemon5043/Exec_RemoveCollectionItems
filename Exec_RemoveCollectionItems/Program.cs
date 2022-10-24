1b6b479e16e69cecfbce6ddf310400b1a05d6328
伯翰 侯 <john860703@gmail.com>
2022-10-24 17:21:02 +0800

    加入專案檔案。

---
+++ Exec_RemoveCollectionItems/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_RemoveCollectionItems
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> items1 = new List<int>() { 3, 8, 6, 7, 5 };
+
+			items1.RemoveRange(2, 3);
+			foreach (var item in items1)
+			{
+				Console.WriteLine(item); //print 3 8
+			}
+		}
+	}
+}
